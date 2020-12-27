d9d6e3323b82bae69e583269643a0016fb44ac16
YanaTrifonova <trifonova.yana.prof@gmail.com>
2020-12-27 18:56:18 +0100

    errorCatcher fixed

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -40,7 +40,7 @@ export const signUp = (name, email, password) => {
             dispatch(showMessageWithTimeout("success", true, "account created"));
             dispatch(appDoneLoading());
         } catch (error) {
-            errorCatcher(error);
+            errorCatcher(error, dispatch);
         }
     };
 };
@@ -58,12 +58,12 @@ export const login = (email, password) => {
             dispatch(showMessageWithTimeout("success", false, "welcome back!", 1500));
             dispatch(appDoneLoading());
         } catch (error) {
-            errorCatcher(error);
+            errorCatcher(error, dispatch);
         }
     };
 };
 
-function errorCatcher(error) {
+function errorCatcher(error, dispatch) {
     if (error.response) {
         console.log(error.response.data.message);
         dispatch(setMessage("danger", true, error.response.data.message));
