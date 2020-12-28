a190649386afbefa043b3ed15fa11c1b3e8b713b
YanaTrifonova <trifonova.yana.prof@gmail.com>
2020-12-28 20:50:51 +0100

    refactoring

--- src/pages/Home/notePlayer.js
+++ src/pages/Home/notePlayer.js
@@ -1,41 +1,4 @@
-export function notePlayer(noteType, aNote, bNote, cNote, dNote, eNote, fNote, gNote) {
-    switch (noteType) {
-        case "a" : {
-            aNote.currentTime = 0;
-            aNote.play();
-            break;
-        }
-        case "b" : {
-            bNote.currentTime = 0;
-            bNote.play();
-            break;
-        }
-        case "c" : {
-            cNote.currentTime = 0;
-            cNote.play();
-            break;
-        }
-        case "d" : {
-            dNote.currentTime = 0;
-            dNote.play();
-            break;
-        }
-        case "e" : {
-            eNote.currentTime = 0;
-            eNote.play();
-            break;
-        }
-        case "f" : {
-            fNote.currentTime = 0;
-            fNote.play();
-            break;
-        }
-        case "g" : {
-            gNote.currentTime = 0;
-            gNote.play();
-            break;
-        }
-        default :
-            return;
-    }
+export function notePlayer(note) {
+    note.currentTime = 0;
+    note.play();
 }
