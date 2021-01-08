3136b5100449afd6cba49744411464cc2eef53ea
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-08 15:53:30 +0100

    updates for project demo

--- src/pages/Discover/index.js
+++ src/pages/Discover/index.js
@@ -1,11 +1,31 @@
-import {Jumbotron} from "react-bootstrap";
+import {Button, Card, Jumbotron} from "react-bootstrap";
 import React from "react";
+import cat from '../../images/default/cat_default_close_mouth.jpg'
 
 export default function Discover() {
 
     return(
-        <Jumbotron>
+        <>
+            <Jumbotron>
             <h1>Discover songs</h1>
         </Jumbotron>
+            <Card style={{ width: '18rem', margin: "20px" }}>
+                <Card.Img variant="top" src={cat} />
+                <Card.Body>
+                    <Card.Title>My first song</Card.Title>
+                    <Card.Text>
+                        I created my first song. Feel free to listen to it!
+                    </Card.Text>
+                    <Card.Body>
+                        <Button variant="primary" style={{ marginRight: "5px" }}>Play</Button>
+                        <Button variant="primary">Stop</Button>
+                    </Card.Body>
+                    <Card.Body>
+                        <Button variant="primary" style={{ marginRight: "5px" }}>Like</Button>
+                        <Button variant="primary">Favorites</Button>
+                    </Card.Body>
+                </Card.Body>
+            </Card>
+        </>
     )
 }
