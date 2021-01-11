35fb52f3ddd2c5719e076db371c578c67fdd6c35
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-11 23:15:05 +0100

    sign up and login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,28 @@
 import React, {useEffect, useState} from "react";
-import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 import {login} from "../../store/user/actions";
 import {selectToken} from "../../store/user/selector";
 import {useDispatch, useSelector} from "react-redux";
-import {Link, useHistory} from "react-router-dom";
-import {Col} from "react-bootstrap";
+import {useHistory} from "react-router-dom";
+import {Button, Container, makeStyles, TextField} from "@material-ui/core";
 
 export default function SignUp() {
+    const useStyles = makeStyles((_) => ({
+        container: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            flexDirection: "column",
+            maxWidth: "400px",
+            margin: "auto",
+        },
+        button: {
+            maxWidth: "fit-content",
+            margin: "auto",
+            marginTop: "30px"
+        }
+    }));
+
+    const classes = useStyles();
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const dispatch = useDispatch();
@@ -33,41 +47,40 @@ export default function SignUp() {
 
     return (
         <Container>
-            <Form as={Col} md={{
-                span: 6,
-                offset: 3
-            }} className="mt-5">
-                <h1 className="mt-5 mb-5">Login</h1>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control
-                        value={email}
-                        onChange={event => setEmail(event.target.value)}
-                        type="email"
-                        placeholder="Enter email"
-                        required
-                    />
-                </Form.Group>
+            <form className={classes.container}>
+                <h1>Login</h1>
+                <TextField
+                    margin="normal"
+                    value={email}
+                    onChange={event => setEmail(event.target.value)}
+                    required
+                    label="email"
+                    type="email"
+                    autoComplete="current-email"
+                    variant="outlined"
+                    id="outlined-email-input"
+                />
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        value={password}
-                        onChange={event => setPassword(event.target.value)}
-                        type="password"
-                        placeholder="Password"
-                        required
-                    />
-                </Form.Group>
-                <Form.Group className="mt-5">
-                    <Button variant="primary" type="submit" onClick={submitForm}>
-                        Log in
-                    </Button>
-                </Form.Group>
-                <Link to="/signup" style={{textAlign: "center"}}>
+                <TextField
+                    margin="normal"
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}
+                    required
+                    id="outlined-password-input"
+                    label="password"
+                    type="password"
+                    autoComplete="current-password"
+                    variant="outlined"
+                />
+
+                <Button className={classes.button} variant="contained" color="primary" type="submit"
+                        onClick={submitForm}>
+                    Log in
+                </Button>
+                <a className={classes.button} href={"/signup"}>
                     Click here to sign up
-                </Link>
-            </Form>
+                </a>
+            </form>
         </Container>
     );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,14 +1,28 @@
 import React, {useEffect, useState} from "react";
-import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 import {signUp} from "../../store/user/actions";
 import {selectToken} from "../../store/user/selector";
 import {useDispatch, useSelector} from "react-redux";
-import {Link, useHistory} from "react-router-dom";
-import {Col} from "react-bootstrap";
+import {useHistory} from "react-router-dom";
+import {Button, Container, makeStyles, TextField} from "@material-ui/core";
 
 export default function SignUp() {
+    const useStyles = makeStyles((_) => ({
+        container: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            flexDirection: "column",
+            maxWidth: "400px",
+            margin: "auto",
+        },
+        button: {
+            maxWidth: "fit-content",
+            margin: "auto",
+            marginTop: "30px"
+        }
+    }));
+
+    const classes = useStyles();
+
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -34,52 +48,53 @@ export default function SignUp() {
 
     return (
         <Container>
-            <Form as={Col} md={{
-                span: 6,
-                offset: 3
-            }} className="mt-5">
-                <h1 className="mt-5 mb-5">Signup</h1>
-                <Form.Group controlId="formBasicName">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                        value={name}
-                        onChange={event => setName(event.target.value)}
-                        type="text"
-                        placeholder="Enter name"
-                        required
-                    />
-                </Form.Group>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control
-                        value={email}
-                        onChange={event => setEmail(event.target.value)}
-                        type="email"
-                        placeholder="Enter email"
-                        required
-                    />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+            <form className={classes.container}>
+                <h1>Signup</h1>
+                <TextField
+                    id="outlined-name-input"
+                    margin="normal"
+                    value={name}
+                    onChange={event => setName(event.target.value)}
+                    type="text"
+                    variant="outlined"
+                    label="name"
+                    autoComplete="current-name"
+                    placeholder="name"
+                    required
+                />
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        value={password}
-                        onChange={event => setPassword(event.target.value)}
-                        type="password"
-                        placeholder="Password"
-                        required
-                    />
-                </Form.Group>
-                <Form.Group className="mt-5">
-                    <Button variant="primary" type="submit" onClick={submitForm}>
-                        Sign up
-                    </Button>
-                </Form.Group>
-                <Link to="/login">Click here to log in</Link>
-            </Form>
+                <TextField
+                    margin="normal"
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}
+                    required
+                    id="outlined-password-input"
+                    label="password"
+                    type="password"
+                    autoComplete="current-password"
+                    variant="outlined"
+                />
+
+                <TextField
+                    margin="normal"
+                    value={email}
+                    onChange={event => setEmail(event.target.value)}
+                    required
+                    label="email"
+                    type="email"
+                    autoComplete="current-email"
+                    variant="outlined"
+                    id="outlined-email-input"
+                    helperText="We'll never share your email with anyone else."
+                />
+                <Button className={classes.button} variant="contained" color="primary" type="submit"
+                        onClick={submitForm}>
+                    Sign up
+                </Button>
+                <a className={classes.button} href={"/login"}>
+                    Click here to log in
+                </a>
+            </form>
         </Container>
     );
 }
