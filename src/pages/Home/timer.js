7cf6eb3e1c1bc3e21967fbb8843ad69313bd0ed9
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-13 14:52:17 +0100

    added timer for recording

---
+++ src/pages/Home/timer.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from "react";
+
+export default function Timer(props) {
+    const [second, setSecond] = useState('00');
+    const [minute, setMinute] = useState('00');
+    const [counter, setCounter] = useState(0);
+
+    const isActive = props.isActive;
+
+    useEffect(() => {
+        let intervalId;
+
+        if (isActive) {
+            intervalId = setInterval(() => {
+                const secondCounter = counter % 60;
+                const minuteCounter = Math.floor(counter / 60);
+
+                const computedSecond = String(secondCounter).length === 1 ? `0${secondCounter}`: secondCounter;
+                const computedMinute = String(minuteCounter).length === 1 ? `0${minuteCounter}`: minuteCounter;
+
+                setSecond(computedSecond);
+                setMinute(computedMinute);
+
+                setCounter(counter => counter + 1);
+            }, 1000)
+        }
+
+        return () => clearInterval(intervalId);
+    }, [isActive, counter])
+
+    return(
+        <div className="timer">
+            <span className="minute">{minute}</span>
+            <span>:</span>
+            <span className="second">{second}</span>
+        </div>
+    );
+}
