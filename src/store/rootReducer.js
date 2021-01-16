3896793e41a65750425db222d5ec800ec739af1b
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-16 21:04:27 +0100

    added preload media reducer (store images and instruments urls in localStorage)

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,9 +2,11 @@ import {combineReducers} from "redux";
 import appState from "./appState/reducer";
 import user from "./user/reducer";
 import record from "./record/reducer";
+import preLoadMedia from "./preLoadMedia/reducer";
 
 export default combineReducers({
     appState,
     user,
     record,
+    preLoadMedia,
 });
