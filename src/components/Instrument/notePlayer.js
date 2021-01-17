cfff080f91072bf3f78f8be65aa1b7cc93fd9b5b
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-17 20:49:44 +0100

    added some statements for different type of instruments

--- src/components/Instrument/notePlayer.js
+++ src/components/Instrument/notePlayer.js
@@ -2,7 +2,17 @@ export function notePlayer(note) {
     note.currentTime = 0;
     note.play();
 
-    setTimeout(() => {
-        note.pause();
-    }, 3000)
+    if (note.id.includes("sadViolin") || note.id.includes("dunDunDun")) {
+        setTimeout(() => {
+            note.pause();
+        }, 1300)
+    } if ( note.id.includes("electricSaw"))  {
+        setTimeout(() => {
+            note.pause();
+        }, 2000)
+    } else {
+        setTimeout(() => {
+            note.pause();
+        }, 3000)
+    }
 }
