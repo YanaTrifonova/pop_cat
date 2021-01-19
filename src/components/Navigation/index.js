0b6ecfb3dbd5725fcfb4ead6129877f9f42ad7df
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-19 11:07:56 +0100

    added options to hide navbar links for user that is not logged in

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -22,9 +22,9 @@ export default function Navigation() {
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav style={{width: "100%"}} fill>
                     <NavbarItem path="/" linkText="Home"/>
-                    <NavbarItem path="/private" linkText="My songs"/>
+                    {token === null ? null : <NavbarItem path="/private" linkText="My songs"/>}
                     <NavbarItem path="/discover" linkText="Discover"/>
-                    <NavbarItem path="/favorites" linkText="Favorites"/>
+                    {token === null ? null : <NavbarItem path="/favorites" linkText="Favorites"/>}
                     <NavbarItem path="/about" linkText="About Me"/>
                     {loginLogoutControls}
                 </Nav>
