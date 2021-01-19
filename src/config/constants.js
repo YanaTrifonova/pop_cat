38588013e4e73d1b20b0c608fd427e879c5e85b9
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-19 12:12:58 +0100

    typo fixed

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-export const apiUrl = process.env.NODE_ENV === "production" ? 'https://pop-cat.herokuapp.com/' : "http://localhost:4000";
+export const apiUrl = process.env.NODE_ENV === "production" ? 'https://pop-cat.herokuapp.com' : "https://pop-cat.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
