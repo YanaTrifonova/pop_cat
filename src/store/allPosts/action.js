7a6cccafbefab6e6fa86aa407cdc3ff5d682fa62
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-21 18:33:37 +0100

    added userId as parameter to request

--- src/store/allPosts/action.js
+++ src/store/allPosts/action.js
@@ -10,10 +10,17 @@ function getAllPosts(data) {
     }
 }
 
-export const getData = () => {
+export const getData = (userId) => {
     return async (dispatch) => {
         try {
-            const response = await axios.get(`${apiUrl}/songs`);
+            const response = await axios.get(
+                `${apiUrl}/songs`,
+                {
+                    headers: {
+                        userId: userId,
+                    }
+                },
+            );
             dispatch(getAllPosts(response.data));
         } catch (e) {
             console.log(e);
