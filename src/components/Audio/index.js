41c4a5b65db3910e1a1a6b97b9f93fa1d3395a19
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-01-22 10:50:40 +0100

    changed order of imports of audio files

--- src/components/Audio/index.js
+++ src/components/Audio/index.js
@@ -5,12 +5,35 @@ export default function Audio(props) {
     const instrument = props.instrument;
 
     return (
-        instrument.notes.map((note, index) => {
-            return (
-                <audio id={instrument.name + "Note" + index} key={index}>
-                    <source src={apiUrl + note} type="audio/mpeg"/>
-                </audio>
-            )
-        })
+        <>
+            {/*C*/}
+            <audio id={instrument.name + "Note" + 0} key={0}>
+                <source src={apiUrl + instrument.notes[2]} type="audio/mpeg"/>
+            </audio>
+            {/*D*/}
+            <audio id={instrument.name + "Note" + 1} key={1}>
+                <source src={apiUrl + instrument.notes[3]} type="audio/mpeg"/>
+            </audio>
+            {/*E*/}
+            <audio id={instrument.name + "Note" + 2} key={2}>
+                <source src={apiUrl + instrument.notes[4]} type="audio/mpeg"/>
+            </audio>
+            {/*F*/}
+            <audio id={instrument.name + "Note" + 3} key={3}>
+                <source src={apiUrl + instrument.notes[5]} type="audio/mpeg"/>
+            </audio>
+            {/*G*/}
+            <audio id={instrument.name + "Note" + 4} key={4}>
+                <source src={apiUrl + instrument.notes[6]} type="audio/mpeg"/>
+            </audio>
+            {/*A*/}
+            <audio id={instrument.name + "Note" + 5} key={5}>
+                <source src={apiUrl + instrument.notes[1]} type="audio/mpeg"/>
+            </audio>
+            {/*B*/}
+            <audio id={instrument.name + "Note" + 6} key={6}>
+                <source src={apiUrl + instrument.notes[2]} type="audio/mpeg"/>
+            </audio>
+        </>
     )
 }
